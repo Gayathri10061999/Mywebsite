1a8848b2ec75d05ce24c690c357dcf3f7f92edab
Gayathri10061999 <92244699+Gayathri10061999@users.noreply.github.com>
2022-10-22 12:55:08 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,7 @@
+var xhr=new XMLHttpRequest();
+xhr.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
+xhr.send();
+xhr.onload=function(){
+  let data=JSON.parse(this.response);
+  console.log(data);
+}
